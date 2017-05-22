7e062de45edfb5aaac4505fdf02937f7651bdc50
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2017-05-22 11:06:41 +0200

    correction

--- libs/bragi/src/query.rs
+++ libs/bragi/src/query.rs
@@ -92,6 +92,7 @@ fn build_query(q: &str,
                -> rs_es::query::Query {
 
     use rs_es::query::functions::Function;
+    let boost_ad_label = 50;
     // we order the type of object we want
     // Note: the addresses are boosted more because even if we don't want them first
     // because they are more severely filtered
@@ -144,12 +145,12 @@ fn build_query(q: &str,
 
         should_query.push(
             rs_q::build_match("administrative_regions.label".to_string(), q.to_string())
-            .with_boost(50)
+            .with_boost(boost_ad_label)
             .build()
         );
         should_query.push(rs_q::build_match("street.administrative_regions.label".to_string(),
                                             q.to_string())
-            .with_boost(50)
+            .with_boost(boost_ad_label)
             .build())
     }
 
